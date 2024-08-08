17a413eff8cddca872952ab71d1ff229847d670d
Kaasklontje <84289148+Kaasklontje@users.noreply.github.com>
2024-08-08 11:32:53 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kaasklontje'
+description 'Log player events and actions to Fivemanage'
+version '1.0.0'
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
