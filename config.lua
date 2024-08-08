0c80571e3ebdf6171a53dec87635a322848a21ba
Kaasklontje <84289148+Kaasklontje@users.noreply.github.com>
2024-08-08 11:33:21 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,6 @@
+-- Initialize the Config table
+Config = {}
+
+-- Set the apiKey and apiUrl
+Config.apiKey = 'API KEY HERE'  -- Verander met API token
+Config.apiUrl = 'https://api.fivemanage.com/api/logs' -- Gewoon zo laten
